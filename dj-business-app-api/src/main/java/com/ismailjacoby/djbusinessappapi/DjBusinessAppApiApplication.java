2ff4790b28531feaeec3c9aeba7e61d6d5779370
Ismail <ismail.jacoby@gmail.com>
2025-03-26 16:49:32 +0100

    Backend & Frontend - Initial commit

---
+++ dj-business-app-api/src/main/java/com/ismailjacoby/djbusinessappapi/DjBusinessAppApiApplication.java
@@ -0,0 +1,13 @@
+package com.ismailjacoby.djbusinessappapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DjBusinessAppApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DjBusinessAppApiApplication.class, args);
+	}
+
+}
---
+++ dj-business-app-api/src/test/java/com/ismailjacoby/djbusinessappapi/DjBusinessAppApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ismailjacoby.djbusinessappapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DjBusinessAppApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

fbcdefb8302195908fcac73f291e463143b39759
Ismail <ismail.jacoby@gmail.com>
2025-03-26 19:50:20 +0100

    Frontend: Responsive Navbar

--- dj-business-app-frontend/src/app/components/shared/header/header.component.ts
+++ dj-business-app-frontend/src/app/components/shared/header/header.component.ts
@@ -1,12 +1,33 @@
 import { Component } from '@angular/core';
+import {RouterLink, RouterLinkActive} from '@angular/router';
+import {NgClass} from "@angular/common";
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink,
+    RouterLinkActive,
+    NgClass
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+  isLoggedIn: boolean = true;
+  isMenuOpen: boolean = false;
+
+  login(){
+    this.isLoggedIn = true;
+  }
+
+  logout(){
+    this.isLoggedIn = false;
+    this.isMenuOpen = false;
+  }
+
+  toggleMenu(){
+    this.isMenuOpen = !this.isMenuOpen;
+  }
 
 }

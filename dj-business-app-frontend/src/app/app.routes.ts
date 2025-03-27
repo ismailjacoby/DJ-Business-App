b708e34e35e5859197a9b6b88ae8212c04731e26
Ismail <ismail.jacoby@gmail.com>
2025-03-27 12:19:17 +0100

    Frontend: Placeholder Dashboard created

--- dj-business-app-frontend/src/app/app.routes.ts
+++ dj-business-app-frontend/src/app/app.routes.ts
@@ -1,5 +1,7 @@
 import {Routes} from '@angular/router';
+import {DashboardComponent} from './components/dashboard/dashboard.component';
 
 export const routes: Routes = [
+  {path:'dashboard', component: DashboardComponent},
   {path: 'auth', loadChildren: () => import('./components/auth/auth.routes').then(m => m.authRoutes)}
 ];
